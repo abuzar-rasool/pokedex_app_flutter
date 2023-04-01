03530519409cf1b941afef1228472fdc74bf316a
Abuzar Rasool <abuzar@oraan.com>
2023-04-01 22:37:30 +0500

    Deleted very good core project

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.pokedex_app_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main_staging.dart
+++
@@ -1,6 +0,0 @@
-import 'package:pokedex_app_flutter/app/app.dart';
-import 'package:pokedex_app_flutter/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
